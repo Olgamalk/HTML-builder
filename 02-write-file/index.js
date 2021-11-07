a717b95eeb9f50394a6d75487330b8cdc3741c46
Olga Malkovich <olga.malkovich@gmail.com>
2021-11-07 22:05:32 +0300

    fix: add some solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,14 @@
+let fs = require('fs');
 let readline = require('readline');
 
 let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+let writeableStream = fs.createWriteStream("some.txt");
+writeableStream.write('Hello world!');
+writeableStream.end('End!');
 
 rl.question('What do you add in the file?', function(answer) {
     console.log('Thanks for your answer:', answer);
