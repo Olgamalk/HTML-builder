f4ac0d07d87f8cf4a5b3c6d3b11b45857bdcfebb
Olga Malkovich <olga.malkovich@gmail.com>
2022-05-19 15:58:29 +0300

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,3 +5,16 @@ fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
   if (err) throw err;
   console.log('Folder was created');
 });
+
+
+fs.readdir(path.join(__dirname, 'files'), 'utf-8', (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    files.forEach(file => {
+      fs.copyFile(`${path.dirname(__filename)}/files/${file}`, `${path.dirname(__filename)}/files-copy/${file}`, err => {
+        if (err) throw err;
+      })
+    });
+  }
+});
