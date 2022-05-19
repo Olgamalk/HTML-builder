4b6fedb3ac4438d5c22820fbd9184548a4dba6ae
Olga Malkovich <olga.malkovich@gmail.com>
2022-05-19 15:27:48 +0300

    feat: add some code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+  if (err) throw err;
+  console.log('Folder was created');
+});
