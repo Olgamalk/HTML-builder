3fbee169c62a2695a04e52b203df62860742a30f
Olga Malkovich <olga.malkovich@gmail.com>
2022-05-25 21:48:31 +0300

    fix: update solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+fs.promises.rm(path.join(__dirname, 'files-copy'), { recursive: true, force:true }, err => {
   if (err) throw err;
-  console.log('Folder was created');
-});
+}).then(() => {
+  copyFolder();
+})
 
-
-fs.readdir(path.join(__dirname, 'files'), 'utf-8', (err, files) => {
-  if (err) {
-    throw err;
-  } else {
+const copyFolder = async () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+    if (err) throw err;
+  });
+  
+  fs.readdir(`${path.join(__dirname, 'files')}`, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
     files.forEach(file => {
-      fs.copyFile(`${path.dirname(__filename)}/files/${file}`, `${path.dirname(__filename)}/files-copy/${file}`, err => {
+      fs.copyFile(`${path.join(__dirname, 'files', `${file.name}`)}`, `${path.join(__dirname, 'files-copy', `${file.name}`)}`, (err) => {
         if (err) throw err;
       })
-    });
-  }
-});
+    })
+  })
+}
